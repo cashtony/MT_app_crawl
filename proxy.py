a706b6efe699dc08cd7cd79b5e943ec9b3819da4
unknown <sw00384@SHUWEI.COM>
2020-05-22 20:27:48 +0800

    1

--- proxy.py
+++ proxy.py
@@ -33,7 +33,7 @@ def abuyun():
 def taiyang_proxy():
     resp = requests.get('http://http.tiqu.qingjuhe.cn/getip?num=1&type=1&pack=20681&port=11&lb=4&pb=45&regions=')
     # print(resp.text)
-    if '请求' in resp.text:
+    if '套餐已用完' in resp.text or '频繁' in resp.text:
         resp = requests.get('http://http.tiqu.qingjuhe.cn/getip?num=1&type=1&pack=50186&port=1&lb=1&pb=4&regions=')
     proxy =  {
         'http':'http://'+resp.text,
@@ -41,4 +41,6 @@ def taiyang_proxy():
     }
     return proxy
 
-
+if __name__ == '__main__':
+    resp = requests.get('http://http.tiqu.qingjuhe.cn/getip?num=1&type=1&pack=20681&port=11&lb=4&pb=45&regions=')
+    print(resp.text)
